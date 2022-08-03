89bd056b3224d5e6278535f6cf5b97839076568c
MachineLearningMike <fleetpro@gmail.com>
2022-08-04 05:45:25 +0800

    for-test addred

---
+++ contracts/for-test/Keep3rHelperForTest.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+import '../agency/Keep3rHelper.sol';
+
+contract Keep3rHelperForTest is Keep3rHelper {
+  uint256 public basefee;
+
+  constructor(address _keep3rV2, address _governance) Keep3rHelper(_keep3rV2, _governance) {}
+
+  function _getBasefee() internal view override returns (uint256) {
+    return basefee;
+  }
+
+  function setBaseFee(uint256 _baseFee) external {
+    basefee = _baseFee;
+  }
+}
---
+++ contracts/for-test/UniV3PairManagerForTest.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+
+import '../agency/libraries/LiquidityAmounts.sol';
+import '../agency/libraries/PoolAddress.sol';
+import '../agency/libraries/FixedPoint96.sol';
+import '../agency/libraries/FullMath.sol';
+import '../agency/libraries/TickMath.sol';
+import '../agency/UniV3PairManager.sol';
+import '../interfaces/external/IWeth9.sol';
+import '../interfaces/IUniV3PairManager.sol';
+
+contract UniV3PairManagerForTest is UniV3PairManager {
+  constructor(address _pool, address _governance) UniV3PairManager(_pool, _governance) {}
+
+  function internalAddLiquidity(
+    uint256 amount0Desired,
+    uint256 amount1Desired,
+    uint256 amount0Min,
+    uint256 amount1Min
+  )
+    external
+    returns (
+      uint128 liquidity,
+      uint256 amount0,
+      uint256 amount1
+    )
+  {
+    return _addLiquidity(amount0Desired, amount1Desired, amount0Min, amount1Min);
+  }
+
+  function internalPay(
+    address token,
+    address payer,
+    address recipient,
+    uint256 value
+  ) external {
+    return _pay(token, payer, recipient, value);
+  }
+
+  function internalMint(address dst, uint256 amount) external {
+    return _mint(dst, amount);
+  }
+
+  function internalBurn(address dst, uint256 amount) external {
+    return _burn(dst, amount);
+  }
+
+  function internalTransferTokens(
+    address src,
+    address dst,
+    uint256 amount
+  ) external {
+    _transferTokens(src, dst, amount);
+  }
+
+  function internalSafeTransferFrom(
+    address token,
+    address from,
+    address to,
+    uint256 value
+  ) external {
+    _safeTransferFrom(token, from, to, value);
+  }
+
+  receive() external payable {}
+}
